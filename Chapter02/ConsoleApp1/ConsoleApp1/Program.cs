6fec7e594ad207d93cae07566a8016c7a9acd2aa
Benjamin Perkins <benperk@microsoft.com>
2020-10-16 19:28:00 +0200

    Add files via upload

---
+++ Chapter02/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Output text to the screen.
+            Console.WriteLine("The first app in Beginning C# and .NET!");
+            Console.ReadKey();
+        }
+    }
+}
