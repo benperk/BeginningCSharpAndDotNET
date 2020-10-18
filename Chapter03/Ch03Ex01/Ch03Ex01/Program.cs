803116f8423d655f1aaf21e53141b0223a231e5c
Benjamin Perkins <benperk@microsoft.com>
2020-10-18 12:50:49 +0200

    Add files via upload

---
+++ Chapter03/Ch03Ex01/Ch03Ex01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Ch03Ex01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myInteger;
+            string myString;
+            myInteger = 17;
+            myString = "\"myInteger\" is";
+            Console.WriteLine($"{myString} {myInteger}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Chapter03/Ch03Ex02/Ch03Ex02/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Ch03Ex02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNumber, secondNumber;
+            string userName;
+            Console.WriteLine("Enter your name:");
+            userName = Console.ReadLine();
+            Console.WriteLine($"Welcome {userName}!");
+            Console.WriteLine("Now give me a number:");
+            firstNumber = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Now give me another number:");
+            secondNumber = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"The sum of {firstNumber} and {secondNumber} is " +
+                          $"{firstNumber + secondNumber}.");
+            Console.WriteLine($"The result of subtracting {secondNumber} from " +
+                          $"{firstNumber} is {firstNumber - secondNumber}.");
+            Console.WriteLine($"The product of {firstNumber} and {secondNumber} " +
+                          $"is {firstNumber * secondNumber}.");
+            Console.WriteLine($"The result of dividing {firstNumber} by " +
+                          $"{secondNumber} is {firstNumber / secondNumber}.");
+            Console.WriteLine($"The remainder after dividing {firstNumber} by " +
+                          $"{secondNumber} is {firstNumber % secondNumber}.");
+            Console.ReadKey();
+
+        }
+    }
+}
