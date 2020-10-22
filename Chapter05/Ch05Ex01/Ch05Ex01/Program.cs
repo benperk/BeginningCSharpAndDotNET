e30893086c7f13c371811c026800c0b21efa90e6
Benjamin Perkins <benperk@microsoft.com>
2020-10-22 19:58:58 +0200

    Add files via upload

---
+++ Chapter05/Ch05Ex01/Ch05Ex01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Ch05Ex01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            short shortResult, shortVal = 4;
+            int integerVal = 67;
+            long longResult;
+            float floatVal = 10.5F;
+            double doubleResult, doubleVal = 99.999;
+            string stringResult, stringVal = "17";
+            bool boolVal = true;
+            Console.WriteLine("Variable Conversion Examples\n");
+            doubleResult = floatVal * shortVal;
+            Console.WriteLine($"Implicit, -> double: {floatVal} * {shortVal} -> { doubleResult }");
+            shortResult = (short)floatVal;
+            Console.WriteLine($"Explicit, -> short:  {floatVal} -> {shortResult}");
+            stringResult = Convert.ToString(boolVal) +
+               Convert.ToString(doubleVal);
+            Console.WriteLine($"Explicit, -> string: \"{boolVal}\" + \"{doubleVal}\" -> " +
+                      $"{stringResult}");
+            longResult = integerVal + Convert.ToInt64(stringVal);
+            Console.WriteLine($"Mixed, -> long:   {integerVal} + {stringVal} -> {longResult}");
+            Console.ReadKey();
+        }
+    }
+}
