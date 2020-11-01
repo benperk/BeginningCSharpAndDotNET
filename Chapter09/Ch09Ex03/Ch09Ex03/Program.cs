cc6d857d54e2861276bc71bc7a32d67da25da7e9
Benjamin Perkins <benperk@microsoft.com>
2020-11-01 20:18:20 +0100

    Add files via upload

---
+++ Chapter09/Ch09Ex03/Ch09Ex03/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Ch09Ex03
+{
+    class MyClass
+    {
+        public int val;
+    }
+    struct myStruct
+    {
+        public int val;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass objectA = new MyClass();
+            MyClass objectB = objectA;
+            objectA.val = 10;
+            objectB.val = 20;
+            myStruct structA = new myStruct();
+            myStruct structB = structA;
+            structA.val = 30;
+            structB.val = 40;
+            Console.WriteLine($"objectA.val = {objectA.val}");
+            Console.WriteLine($"objectB.val = {objectB.val}");
+            Console.WriteLine($"structA.val = {structA.val}");
+            Console.WriteLine($"structB.val = {structB.val}");
+            Console.ReadKey();
+        }
+    }
+}
