1d19f1a268aa23b81f2d43cac1b825b7391a6799
Benjamin Perkins <benperk@microsoft.com>
2020-11-03 17:49:57 +0100

    Add files via upload

---
+++ Chapter10/Ch10CardLib/Ch10CardLib/Card.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ch10CardLib
+{
+    public class Card
+    {
+        public Rank rank;
+        public Suit suit;
+
+        private Card() { }
+        public Card(Suit newSuit, Rank newRank)
+        {
+            suit = newSuit;
+            rank = newRank;
+        }
+        public override string ToString() => "The " + rank + " of " + suit + "s";
+    }
+}
---
+++ Chapter10/Ch10Ex01/Ch10Ex01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ch10Ex01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Creating object myObj...");
+            MyClass myObj = new MyClass("My Object");
+            Console.WriteLine("myObj created.");
+            for (int i = -1; i <= 0; i++)
+            {
+                try
+                {
+                    Console.WriteLine($"\nAttempting to assign {i} to myObj.Val...");
+                    myObj.Val = i;
+                    Console.WriteLine($"Value {myObj.Val} assigned to myObj.Val.");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Exception {e.GetType().FullName} thrown.");
+                    Console.WriteLine($"Message:\n\"{e.Message}\"");
+                }
+            }
+            Console.WriteLine("\nOutputting myObj.ToString()...");
+            Console.WriteLine(myObj.ToString());
+            Console.WriteLine("myObj.ToString() Output.");
+            Console.WriteLine("\nmyDoubledIntProp = 5...");
+            Console.WriteLine($"Getting myDoubledIntProp of 5 is {myObj.myDoubledIntProp}");
+            Console.ReadKey();
+        }
+    }
+}
