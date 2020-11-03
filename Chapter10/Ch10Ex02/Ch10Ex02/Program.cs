91767c3056e984df59a184c5f66f26ca056f0168
Benjamin Perkins <benperk@microsoft.com>
2020-11-03 17:50:44 +0100

    Add files via upload

---
+++ Chapter10/Ch10Ex02/Ch10Ex02/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Ch10Ex02
+{
+    public class ClassA
+    {
+        private int state = -1;
+        public int State => state;
+        public class ClassB
+        {
+            public void SetPrivateState(ClassA target, int newState)
+            {
+                target.state = newState;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassA myObject = new ClassA();
+            Console.WriteLine($"myObject.State = {myObject.State}");
+            ClassA.ClassB myOtherObject = new ClassA.ClassB();
+            myOtherObject.SetPrivateState(myObject, 999);
+            Console.WriteLine($"myObject.State = {myObject.State}");
+            Console.ReadKey();
+        }
+    }
+}
