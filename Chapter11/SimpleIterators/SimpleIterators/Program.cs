6d922b5e4de807aa7e0c789a09ef6970bd296a34
Benjamin Perkins <benperk@microsoft.com>
2020-11-08 16:15:38 +0100

    Add files via upload

---
+++ Chapter11/SimpleIterators/SimpleIterators/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+
+namespace SimpleIterators
+{
+    class Program
+    {
+        public static IEnumerable SimpleList()
+        {
+            yield return "string 1";
+            yield return "string 2";
+            yield return "string 3";
+        }
+        static void Main(string[] args)
+        {
+            foreach (string item in SimpleList())
+                Console.WriteLine(item);
+            Console.ReadKey();
+        }
+    }
+}
