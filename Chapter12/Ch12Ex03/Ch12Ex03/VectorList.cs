774682abc6a9aec7c1bcbdc45e95d3c5745148c6
Benjamin Perkins <benperk@microsoft.com>
2021-02-01 12:10:46 +0100

    Add files via upload

---
+++ Chapter12/Ch12Ex03/Ch12Ex03/VectorList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ch12Ex03
+{
+    public class VectorList : List<Vector>
+    {
+        public VectorList() : base() { }
+        public VectorList(IEnumerable<Vector> initialItems)
+        {
+            foreach (Vector vector in initialItems)
+            {
+                Add(vector);
+            }
+        }
+        public string Sum()
+        {
+            StringBuilder sb = new StringBuilder();
+            Vector currentPoint = new Vector(0.0, 0.0);
+            sb.Append("origin");
+            foreach (Vector vector in this)
+            {
+                sb.AppendFormat($" + {vector}");
+                currentPoint += vector;
+            }
+            sb.AppendFormat($" = {currentPoint}");
+            return sb.ToString();
+        }
+    }
+}
