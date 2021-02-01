e615f723285ae13ee6adc2fecc60de59c2c617e2
Benjamin Perkins <benperk@microsoft.com>
2021-02-01 10:58:53 +0100

    Add files via upload

---
+++ Chapter11/Ch11Ex05/Ch11Ex05/PersonNameComparer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+namespace Ch11Ex05
+{
+    public class PersonNameComparer : IComparer
+    {
+        public static IComparer Default = new PersonNameComparer();
+        public int Compare(object x, object y)
+        {
+            if (x is Person && y is Person)
+            {
+                return Comparer.Default.Compare(
+                   ((Person)x).Name, ((Person)y).Name);
+            }
+            else
+            {
+                throw new ArgumentException(
+                   "One or both objects to compare are not Person objects.");
+            }
+        }
+    }
+}
