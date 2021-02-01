5ccc4b59f19964f62b0e1350f6173a742464cf1a
Benjamin Perkins <benperk@microsoft.com>
2021-02-01 10:42:44 +0100

    Update Program.cs

--- Chapter11/Ch11CardClient/Ch11CardClient/Program.cs
+++ Chapter11/Ch11CardClient/Ch11CardClient/Program.cs
@@ -18,18 +18,18 @@ namespace Ch11CardClient
             card3 = new Card(Suit.Club, Rank.Ace);
             card4 = new Card(Suit.Heart, Rank.Ten);
             card5 = new Card(Suit.Diamond, Rank.Ace);
-            Console.WriteLine($"{card1.ToString()} == {card2.ToString()} ? {card1 == card2}");
-            Console.WriteLine($"{card1.ToString()} != {card3.ToString()} ? {card1 != card3}");
-            Console.WriteLine($"{card1.ToString()}.Equals({card4.ToString()}) ? " +
+            Console.WriteLine($"{card1} == {card2} ? {card1 == card2}");
+            Console.WriteLine($"{card1)} != {card3} ? {card1 != card3}");
+            Console.WriteLine($"{card1}.Equals({card4}) ? " +
                           $" { card1.Equals(card4)}");
-            Console.WriteLine($"Card.Equals({card3.ToString()}, {card4.ToString()}) ? " +
+            Console.WriteLine($"Card.Equals({card3}, {card4}) ? " +
                   $" { Card.Equals(card3, card4)}");
-            Console.WriteLine($"{card1.ToString()} > {card2.ToString()} ? {card1 > card2}");
-            Console.WriteLine($"{card1.ToString()} <= {card3.ToString()} ? {card1 <= card3}");
-            Console.WriteLine($"{card1.ToString()} > {card4.ToString()} ? {card1 > card4}");
-            Console.WriteLine($"{card4.ToString()} > {card1.ToString()} ? {card4 > card1}");
-            Console.WriteLine($"{card5.ToString()} > {card4.ToString()} ? {card5 > card4}");
-            Console.WriteLine($"{card4.ToString()} > {card5.ToString()} ? {card4 > card5}");
+            Console.WriteLine($"{card1} > {card2} ? {card1 > card2}");
+            Console.WriteLine($"{card1} <= {card3} ? {card1 <= card3}");
+            Console.WriteLine($"{card1} > {card4} ? {card1 > card4}");
+            Console.WriteLine($"{card4} > {card1} ? {card4 > card1}");
+            Console.WriteLine($"{card5} > {card4} ? {card5 > card4}");
+            Console.WriteLine($"{card4} > {card5} ? {card4 > card5}");
             Console.ReadKey();
         }
     }
