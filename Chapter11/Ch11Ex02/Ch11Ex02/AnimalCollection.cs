0c4178665e332773017c789916e04d28ab33497e
Benjamin Perkins <benperk@microsoft.com>
2021-02-01 08:55:13 +0100

    Add files via upload

---
+++ Chapter11/Ch11Ex02/Ch11Ex02/AnimalCollection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+
+namespace Ch11Ex02
+{    public class AnimalCollection : CollectionBase
+    {
+        public void Add(Animal newAnimal) =>
+            List.Add(newAnimal);
+        public void Remove(Animal newAnimal) =>
+            List.Remove(newAnimal);
+        public Animal this[int animalIndex]
+        {
+            get { return (Animal)List[animalIndex]; }
+            set { List[animalIndex] = value; }
+        }
+    }
+}
