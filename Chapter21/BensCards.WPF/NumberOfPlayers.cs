633fd30d0b0514208057fb879d90be8c5cd1ac17
Jon Reid <jon.reid@ifsworld.com>
2021-03-07 21:04:26 -0600

    more commits for Chapter 17 and 21

---
+++ Chapter21/BensCards.WPF/NumberOfPlayers.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.ObjectModel;
+
+namespace BensCards.WPF
+{
+    public class NumberOfPlayers : ObservableCollection<int>
+    {
+        public NumberOfPlayers()
+          : base()
+        {
+            Add(2);
+            Add(3);
+            Add(4);
+        }
+    }
+}
