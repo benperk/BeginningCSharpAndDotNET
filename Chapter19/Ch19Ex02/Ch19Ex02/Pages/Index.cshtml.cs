7489a55a37e0435d3b900d4c0951b4570505a546
Benjamin Perkins <benperk@microsoft.com>
2021-03-21 14:43:36 +0100

    Update Index.cshtml.cs

--- Chapter19/Ch19Ex02/Ch19Ex02/Pages/Index.cshtml.cs
+++ Chapter19/Ch19Ex02/Ch19Ex02/Pages/Index.cshtml.cs
@@ -1,16 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Ch19Ex02.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
         public void OnGet()
         {
+
         }
     }
 }
